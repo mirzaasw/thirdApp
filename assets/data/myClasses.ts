a2bb14cb1204a2d50bc285462517ab570f86b2bf
mirzaasw <52796909+mirzaasw@users.noreply.github.com>
2019-07-18 11:31:27 -0400

    Add files via upload

---
+++ assets/data/myClasses.ts
@@ -0,0 +1,8 @@
+import {ClassInfo} from '../../app/classinfo';
+
+export const MYCLASSES: ClassInfo[]=[
+
+    {cname:'SYST24444', room:'G202',dow:'Thurs',tod:'9-11'},
+    {cname:'SYST35300', room:'GB110',dow:'Fri',tod:'10'}
+
+];
